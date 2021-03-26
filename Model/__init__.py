9a06862f2638ee36cd30a067d0ab4a51583a7569
erfanmoghaddam <74089467+erfanmoghaddam@users.noreply.github.com>
2021-03-26 16:12:26 -0400

    Add files via upload

---
+++ Model/__init__.py
@@ -0,0 +1,3 @@
+# python __init__ file
+# init files are required to make Python treat the directories as containing packages;
+# this is done to prevent directories with a common name, such as string, from unintentionally hiding valid modules that occur later on the module search path.
