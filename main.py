827650d406d53d449b37a7bb3a3d55caff684e45
erfanmoghaddam <74089467+erfanmoghaddam@users.noreply.github.com>
2021-04-17 21:22:28 -0400

    Update main.py
    added custom game option

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from tkinter import Tk
 from Model.kakuro import KakuroRandomGame
+from Model.kakuro import KakuroCustomGame
 from View.kakuroUI import KakuroUI
 from View.login import main_account_screen
 
@@ -11,8 +12,7 @@ WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print ("Wrong number of arguments! Enter mode (custom or random) to run in as argument.\n"
-               "Example Usage: python kakuro.py random to run random puzzles\n"
-               "Going forward with random...\n")
+               "Choosing random...\n")
         main_account_screen()
         game = KakuroRandomGame()
         root = Tk()
@@ -26,10 +26,15 @@ if __name__ == '__main__':
         ui = KakuroUI(root, game)
         root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
         root.mainloop()
+    elif sys.argv[1]=='custom':
+        main_account_screen()
+        game = KakuroCustomGame()
+        root = Tk()
+        ui = KakuroUI(root, game)
+        root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
+        root.mainloop()
     else:
-        print ("Wrong number or format of arguments! Enter mode (custom or random) to run in as argument.\n"
-               "Example Usage: python kakuro.py random to run random puzzles\n"
-               "Going forward with random...\n")
+        print ("Choosing random mode to start the game.")
         main_account_screen()
         game = KakuroRandomGame()
         root = Tk()
